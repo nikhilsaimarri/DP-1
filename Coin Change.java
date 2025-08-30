63c2878a3723d0c41f3b972ecfdba5e25ee412d8
Nikhil_sai <118588515+nikhilsaimarri@users.noreply.github.com>
2025-08-30 01:20:52 -0500

    Update Coin Change.java
    Added Tabulation

--- Coin Change.java
+++ Coin Change.java
@@ -51,3 +51,33 @@ class Solution {
     }
 
 }
+
+
+
+
+
+
+//Tabulation
+// class Solution {
+//     public int coinChange(int[] coins, int amount) {
+//         int[][] memo;
+//         memo = new int[coins.length + 1][amount + 1];
+//         for (int i = 1; i <= amount; i++) {
+//             memo[0][i] = 99999;
+//         }
+//         for (int i = 1; i <= coins.length; i++) {
+//             for (int j = 0; j <= amount; j++) {
+//                 if (coins[i - 1] > j) {
+//                     memo[i][j] = memo[i - 1][j];
+//                 } else {
+//                     memo[i][j] = Math.min(memo[i - 1][j], 1 + memo[i][j - coins[i - 1]]);
+//                 }
+//             }
+//         }
+//         if (memo[coins.length][amount] == 99999) return -1;
+//         return memo[coins.length][amount];
+
+//     }
+
+// }
+
